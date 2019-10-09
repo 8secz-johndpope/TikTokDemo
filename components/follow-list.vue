0a5365114a64782e412519c97578195f822f80a4
lcl <444571321@qq.com>
2019-10-10 00:01:42 +0800

    add scrollY

--- components/follow-list.vue
+++ components/follow-list.vue
@@ -10,45 +10,52 @@
 			</scroll-view>
 		</view>
 		<view class="videoList">
-			<view class="videoItem">
-				<view class="authorInfo">
-					<image src="../static/me.jpg" class="authorImg"></image>
-					<text class="authorName">lcl</text>
-					<icon class="iconfont iconshenglvehao share"></icon>
-				</view>
-
-				<view class="videoBox">
-					<text class="title">
-						@抖音小助手 你怎么看
-					</text>
-					<video src="http://localhost/2-3.mp4 " controls class="video" objectFit="contain"></video>
-				</view>
-				<view class="dataBox">
-					<view class="data-left">
-						<text>3小时前</text>
+			<scroll-view scroll-y="true" style="height: 100%;">
+				<view class="videoItem" v-for="(i, k) in 10" :key="k">
+					<view class="authorInfo">
+						<image src="../static/me.jpg" class="authorImg"></image>
+						<text class="authorName">lcl</text>
+						<icon class="iconfont iconshenglvehao share"></icon>
 					</view>
-					<view class="data-right">
-						<icon class="iconfont iconaixin icon">
-							<text>赞</text>
-						</icon>
 
-						<icon class="iconfont iconpinglun icon">
-							<text>评论</text>
-						</icon>
+					<view class="videoBox">
+						<text class="title">
+							@抖音小助手 你怎么看
+						</text>
+						<video src="http://localhost/2-3.mp4 " controls class="video" objectFit="contain"></video>
+						<view class="music-box">
+							<view class="music">
+								我和我的祖国
+							</view>
+						</view>
+					</view>
+					<view class="dataBox">
+						<view class="data-left">
+							<text>3小时前</text>
+						</view>
+						<view class="data-right">
+							<icon class="iconfont iconaixin icon">
+								<text>赞</text>
+							</icon>
 
-						<icon class="iconfont iconfenxiang icon">
-							<text>分享</text>
-						</icon>
+							<icon class="iconfont iconpinglun icon">
+								<text>评论</text>
+							</icon>
+
+							<icon class="iconfont iconfenxiang icon">
+								<text>分享</text>
+							</icon>
+						</view>
+					</view>
+					<view class="comment">
+						<text class="loveNumber">1.1w评论过</text>
+						<view class="commentBox">
+							<icon class="iconfont iconqianbi" style="float: left;"></icon>
+							<input type="text" value="" placeholder="添加评论..." class="inputComment" />
+						</view>
 					</view>
 				</view>
-				<view class="comment">
-					<text class="loveNumber">1.1w评论过</text>
-					<view class="commentBox">
-						<icon class="iconfont iconqianbi" style="float: left;"></icon>
-						<input type="text" value="" placeholder="添加评论..." class="inputComment" />
-					</view>
-				</view>
-			</view>
+			</scroll-view>
 		</view>
 	</view>
 
@@ -112,6 +119,8 @@
 
 	.videoItem {
 		padding: 0 15px;
+		width: 90%;
+		height: 100%;
 	}
 
 	.authorInfo {
@@ -147,7 +156,21 @@
 	.video {
 		width: 85%;
 		height: 100%;
+		z-index: 5;
+	}
 
+	.music-box {
+		z-index: 8;
+		position: relative;
+		bottom: 30px;
+		font-size: 14px;
+		width: 100px;
+		overflow: hidden;
+	}
+
+	.music {
+		width: 200px;
+		animation: XLeft 4s linear infinite;
 	}
 
 	.title {
@@ -192,9 +215,25 @@
 	.commentBox {
 		height: 25px;
 	}
-	
-	.inputComment{
+
+	.inputComment {
 		margin-left: 30px;
 		font-size: 15px;
+	}
+
+	.videoList {
+		width: 100%;
+		height: 100%;
+	}
+
+
+	@keyframes XLeft {
+		0% {
+			transform: translate3d(80%, 0, 0);
+		}
+
+		100% {
+			transform: translate3d(-80%, 0, 0);
+		}
 	}
 </style>
