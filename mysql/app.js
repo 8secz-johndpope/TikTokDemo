881ca326baeb52ed673dde4458472f230e82635f
lcl <444571321@qq.com>
2019-11-07 22:15:27 +0800

    create 生命周期获取mysql的list

--- mysql/app.js
+++ mysql/app.js
@@ -1,22 +1,49 @@
-
-
 // mysql 搜狐镜像  http://mirrors.sohu.com/mysql/
 
 
-(async function(){
+// https://www.npmjs.com/package/mysql2#using-promise-wrapper
+(async function() {
 	const mysql = require('mysql2/promise');
-	
+	const koa = require('koa');
+	const koaStaticCache = require('koa-static-cache');
+	const Router = require('koa-router');
+	const fs = require('fs')
+
+	const app = new koa();
+	const router = new Router();
+	// 静态
+	app.use(koaStaticCache('./static', {
+		prefix: '/static',
+		gzip: true,
+	}))
+
 	// 链接数据库
 	const connection = await mysql.createConnection({
-		host:'localhost',
-		user:'root',
-		password:'123',
-		database:'node',
+		host: 'localhost',
+		user: 'root',
+		password: '123',
+		database: 'node',
+	})
+
+
+	
+	// 添加路由
+	router.get('/', ctx=>{
+		const content = fs.readFileSync('./static/index.html');
+		ctx.body = content.toString();
 	})
 	
-	//query
+	router.get('/list',async ctx=>{
+		// 查询数据
+		let arr = await connection.query('select * from todolist ');	
+		console.log(arr[0]);
+		ctx.body = {
+			code : 0,
+			data : arr[0]
+		};
+	})
 	
-	let arr = await connection.query('select * from todolist ');
-	
-	console.log(arr);
+
+	app.use(router.routes())
+	app.listen(8080);
 })();
