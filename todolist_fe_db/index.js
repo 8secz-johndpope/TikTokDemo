35da5b0943210447e5d7876f8eb23b3a7dce78d2
lcl <444571321@qq.com>
2019-11-07 20:03:16 +0800

    增删改查一整套！

--- todolist_fe_db/index.js
+++ todolist_fe_db/index.js
@@ -7,21 +7,36 @@ const bodyParser = require('koa-bodyparser')
 const app = new koa();
 
 // 静态资源
-app.use(koaStaticCache('./static',{
-	prefix:'/static',
-	gzip:true
+app.use(koaStaticCache('./static', {
+	prefix: '/static',
+	gzip: true
 }))
+app.use(bodyParser())
 
 
 let datas = {
 	appName: "todoList",
-	maxId:3,
-	list: [
-		{id: 1,name: "铅笔",number: 12,price: 1},
-		{id: 2,name: "笔记本",number: 3,price: 2},
-		{id: 3,name: "橡皮",number: 20,price: 0.5},
+	maxId: 3,
+	list: [{
+			id: 1,
+			name: "铅笔",
+			number: 12,
+			price: 1
+		},
+		{
+			id: 2,
+			name: "笔记本",
+			number: 3,
+			price: 2
+		},
+		{
+			id: 3,
+			name: "橡皮",
+			number: 20,
+			price: 0.5
+		},
 	],
-	color:'index.css',
+	color: 'index.css',
 }
 
 
@@ -32,17 +47,69 @@ let datas = {
 
 // 路由
 var router = new Router();
-router.get('/', async ctx=>{
+router.get('/', async ctx => {
 	ctx.body = "hello";
 })
 
 
-router.get('/list', async ctx=>{
+router.get('/list', async ctx => {
 	// ctx.body = {x:1,y:2}; // => {"x":1,"y":2}
 	ctx.body = {
 		code: 0, //  定义错误码，用于前端处理
 		list: datas.list
 	}
+})
+
+// 修改商品信息
+router.post('/change', async ctx => {
+	let data = ctx.request.body;
+	if (data.id < 0) {
+		ctx.body = {
+			code: 1,
+			data: "请传入id"
+		}
+		return;
+	}
+
+	console.log(data);
+
+	let index = datas.list.findIndex(d => d.id == data.id);
+	datas.list[index] = data;
+
+	ctx.body = {
+		code: 0,
+		data: datas.list
+	}
+
+})
+
+// 添加商品
+router.post('/add', async ctx => {
+	let data = ctx.request.body;
+	data.id = ++datas.maxId;
+	
+	
+	datas.list.push(data);
+	
+	ctx.body = {
+		code: 0,
+		data: datas.list
+	}
+
+})
+
+
+// 删除商品
+router.post('/del', async ctx => {
+	let data = ctx.request.body;	
+	
+	datas.list = datas.list.filter(d => d.id != data.id)
+	
+	ctx.body = {
+		code: 0,
+		data: datas.list
+	}
+
 })
 
 
