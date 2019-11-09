610657e83c3d59182f8b7e6e0906006ff3c38bf1
lcl <444571321@qq.com>
2019-11-09 12:45:20 +0800

    数据库版本的增删查改接口！！

--- mysql/app.js
+++ mysql/app.js
@@ -8,6 +8,7 @@
 	const koaStaticCache = require('koa-static-cache');
 	const Router = require('koa-router');
 	const fs = require('fs')
+	const bodyParser = require('koa-bodyparser')
 
 	const app = new koa();
 	const router = new Router();
@@ -16,6 +17,7 @@
 		prefix: '/static',
 		gzip: true,
 	}))
+	app.use(bodyParser());
 
 	// 链接数据库
 	const connection = await mysql.createConnection({
@@ -26,23 +28,104 @@
 	})
 
 
-	
+
 	// 添加路由
-	router.get('/', ctx=>{
+	router.get('/', ctx => {
 		const content = fs.readFileSync('./static/index.html');
 		ctx.body = content.toString();
 	})
-	
-	router.get('/list',async ctx=>{
+
+	router.get('/list', async ctx => {
 		// 查询数据
-		let arr = await connection.query('select * from todolist ');	
+		let arr = await connection.query('select * from todolist ');
 		console.log(arr[0]);
 		ctx.body = {
-			code : 0,
-			data : arr[0]
+			code: 0,
+			data: arr[0]
 		};
 	})
+
+	router.post('/change', async ctx => {
+		const {
+			id,
+			name,
+			price,
+			number
+		} = ctx.request.body;
+		console.log(id);
+
+		// let [data] = await connection.query(
+		// 	` update todolist set name='${name}', price='${price}', number='$number' where id ='$id'`);
+		// 占位符写法：
+		let query = "update todolist set name=?, price=?, number=? where id=?"
+		const value = [name, price, number, id]
+		let [data] = await connection.query(query, value)
+
+		console.log(data)
+		if (data.affectedRows > 0) {
+
+			ctx.body = {
+				code: 0,
+				data: '修改成功'
+			}
+		} else {
+			ctx.body = {
+				code: 1,
+				data: '修改失败'
+			}
+		}
+	})
+
+	router.post('/add', async ctx => {
+		const {
+			name,
+			price,
+			number
+		} = ctx.request.body;
+
+		let [data] = await connection.query(
+			` insert into todolist (name, price, number) values('${name}','${price}','${number}') `);
+		console.log(data)
+		if (data.affectedRows > 0) {
+
+			ctx.body = {
+				code: 0,
+				data: '添加成功'
+			}
+		} else {
+			ctx.body = {
+				code: 1,
+				data: '添加失败'
+			}
+		}
+	})
+
+
+	router.post('/del', async ctx => {
+		const {
+			id
+		} = ctx.request.body;
+		console.log(id);
+
 	
+		let query = "delete from todolist where id=?"
+		const value = [id]
+		let [data] = await connection.query(query, value)
+
+		console.log(data)
+		if (data.affectedRows > 0) {
+
+			ctx.body = {
+				code: 0,
+				data: '删除成功'
+			}
+		} else {
+			ctx.body = {
+				code: 1,
+				data: '删除失败'
+			}
+		}
+	})
 
 	app.use(router.routes())
 	app.listen(8080);
